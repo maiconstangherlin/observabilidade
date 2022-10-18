1358f916818fc7f17eb175f3ea8bb516a2d58ea9
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2022-10-17 23:09:07 -0300

    Testando métricas

--- src/main/java/com/maiconstangherlin/observabilidade/controller/PessoaController.java
+++ src/main/java/com/maiconstangherlin/observabilidade/controller/PessoaController.java
@@ -1,5 +1,7 @@
 package com.maiconstangherlin.observabilidade.controller;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,25 @@ import java.util.List;
 @RequestMapping("/pessoa")
 public class PessoaController {
 
-    @GetMapping
-    List<String> findAll() {
+    private MeterRegistry meterRegistry;
+
+    public PessoaController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+
+        Gauge.builder("total_de_pessoas_online", Math::random)
+                .description("Número total de pessoas online")
+                .register(meterRegistry);
+    }
+
+    @GetMapping("/women")
+    List<String> findAllWomen() {
+        meterRegistry.counter("total_de_chamadas", "sexo", "feminino").increment();
+        return List.of("Fulana", "Ciclana");
+    }
+
+    @GetMapping("/men")
+    List<String> findAllMen() {
+        meterRegistry.counter("total_de_chamadas", "sexo", "masculino").increment();
         return List.of("Fulano", "Ciclano");
     }
 
